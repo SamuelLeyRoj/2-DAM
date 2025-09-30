5580f0428a119810379c13497a171c5b1e5db133
SamuelLeyRoj <samuelleytonrojas@gmail.com>
2025-09-30 12:22:20 +0200

    guardado

---
+++ Desarollo_de_interfaces/ChangeShop/src/app/pantalla/pantalla.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pantalla',
+  templateUrl: './pantalla.component.html',
+  styleUrls: ['./pantalla.component.scss'],
+  standalone: true,
+})
+export class PantallaComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
