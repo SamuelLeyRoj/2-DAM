fdda53748649dd5345364e403738dc28f15d6670
SamuelLeyRoj <samuelleytonrojas@gmail.com>
2025-11-06 14:23:20 +0100

    guardado android studio

--- Programacion_multimedia_y_dispositivos_moviles/EjerciciosEvaluables/app/src/main/java/com/example/ejerciciosevaluables/Ejercicio3_3Activity.kt
+++ Programacion_multimedia_y_dispositivos_moviles/EjerciciosEvaluables/app/src/main/java/com/example/ejerciciosevaluables/Ejercicio3_3Activity.kt
@@ -1,16 +1,13 @@
 package com.example.ejerciciosevaluables
 
-
 import android.app.Activity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-
-class Ejercicio4Activity : Activity() {
+class Ejercicio3_3Activity: Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_ejercicio4)
+        setContentView(R.layout.activity_ejercicio3_3)
         title = "Pantalla Azul"
     }
 }
-
-
